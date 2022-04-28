578d06897aff65d95b2acf409283c2a81228b343
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-28 09:26:56 -0500

    Update basics_player.cs

--- basics_player.cs
+++ basics_player.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    [SerializeField] private Transform groundCheckTransform;
+    [SerializeField] private LayerMask playerMask;
+
     private bool jumpKeyWasPressed;
+    private float horizontalInput;
+    private Rigidbody rigidBodyComponent;
+    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBodyComponent = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame -> keypress, mouseclick, etc belong here. 
@@ -18,12 +25,26 @@ public class Player : MonoBehaviour
         if(Input.GetKeyDown(KeyCode.Space)){
             jumpKeyWasPressed = true;
         }
+
+        horizontalInput = Input.GetAxis("Horizontal");
     }
 
     //FixedUpdate is called once every physics update. Unity updates 100x a second.
     private void FixedUpdate() {
-        if(jumpKeyWasPressed){
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+        if(Physics.OverlapSphere(groundCheckTransform.position, 0.1f, playerMask).Length == 0){
+            return;
         }
+
+        if(jumpKeyWasPressed){
+            rigidBodyComponent.AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = false;
+        }
+        //handle x of x,y,z 
+        rigidBodyComponent.velocity = new Vector3(
+            horizontalInput, 
+            rigidBodyComponent.velocity.y, 
+            0
+        );
     }
+    
 }
