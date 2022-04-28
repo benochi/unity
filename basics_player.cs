80b698d4cfe2e4301064cbf602b9e8ab2fceed43
Dan Politica <73758508+benochi@users.noreply.github.com>
2022-04-28 08:37:43 -0500

    Update basics_player.cs

--- basics_player.cs
+++ basics_player.cs
@@ -4,24 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
+    private bool jumpKeyWasPressed;
     // Start is called before the first frame update
     void Start()
     {
         
     }
 
-    // Update is called once per frame
+    // Update is called once per frame -> keypress, mouseclick, etc belong here. 
     void Update()
     {
         //jump
         if(Input.GetKeyDown(KeyCode.Space)){
-            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+            jumpKeyWasPressed = true;
         }
     }
 
     //FixedUpdate is called once every physics update. Unity updates 100x a second.
     private void FixedUpdate() {
-        
+        if(jumpKeyWasPressed){
+            GetComponent<Rigidbody>().AddForce(Vector3.up * 5, ForceMode.VelocityChange);
+        }
     }
 }
